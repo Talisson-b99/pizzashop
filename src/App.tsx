c61c37c89155b76446802d799476427927f528ce
talisson <talissonb422@gmail.com>
2024-06-25 18:15:12 -0300

    feat: add shadcn and setup initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
