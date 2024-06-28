6b6138f1b396dfae8d8a1fbee2846c2c80fee353
talisson <talissonb422@gmail.com>
2024-06-28 00:21:14 -0300

    feat: ensure that you can register a new restaurant

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,10 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { registerRestaurant } from '@/api/register-restaurant'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -28,14 +30,24 @@ const SignUp = () => {
   })
   const navigate = useNavigate()
 
-  function handleSignUp(data: SignUpForm) {
-    try {
-      console.log(data)
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  })
 
+  async function handleSignUp(data: SignUpForm) {
+    try {
+      const { email, managerName, phone, restaurantName } = data
+
+      await registerRestaurantFn({
+        email,
+        managerName,
+        phone,
+        restaurantName,
+      })
       toast.success('Restaurante cadastrado com sucesso.', {
         action: {
           label: 'Login',
-          onClick: () => navigate('/sign-in'),
+          onClick: () => navigate(`/sign-in?email=${email}`),
         },
       })
     } catch (error) {
